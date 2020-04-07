2943ac8c73f4fad233f72a9c7750e9340aba2169
Antoine <antoine.servolin@gmail.com>
2020-04-07 23:49:30 +0200

    commencement de la modélisation python

--- modelisation.py
+++ modelisation.py
@@ -4,7 +4,32 @@
 import os
 import matplotlib.pyplot as plt 
 
-## lecture du fichier
+## Constantes
+
+t_init = 0
+t_fin = 1 
+dt = 1e-3
+
+## Fonction principale et contraintes
+
+def cout(t):
+	return 1
+
+def f(V):
+
+	Sum = 0
+
+	for (i,v) in V.enumerate(): # Pour chaque pas de temps
+		Sum += sum(v)*c(i*dt)
+
+	return Sum/i
+
+def contrainte_1(V):
+	return sum(V.T) - 1
+
+## Modélisation de la batterie
+
+	## lecture du fichier
 
 path = "donnees-projet-gr1.txt"
 
@@ -31,14 +56,14 @@ for line in content:
 
 Liste_Liste[index].append(temp) ## On ajoute le dernier élément, qui n'est pas détecté (la denière valeur de batterie) / On pourrait aussi rajouter un saut de ligne à la fin du doc
 
-#print(Liste_t)
-#print(Liste_intens)
-#print(Liste_batt)
+# print(Liste_t)
+# print(Liste_intens)
+# print(Liste_batt)
 
-plt.figure()
-plt.xlabel("temps")
-plt.ylabel("valeurs")
-plt.plot(Liste_t,Liste_batt, label = "batterie")
-plt.plot(Liste_t, Liste_intens, label = "intensité")
-plt.legend(loc = 0)
-plt.show()
+# plt.figure()
+# plt.xlabel("temps")
+# plt.ylabel("valeurs")
+# plt.plot(Liste_t,Liste_batt, label = "batterie")
+# plt.plot(Liste_t, Liste_intens, label = "intensité")
+# plt.legend(loc = 0)
+# plt.show()
